052c0c8e09fdf7c492861210f217646118e6b3f9
Noiret <sergio.ko@icloud.com>
2020-10-26 17:41:34 +0100

    Added JS functionality to open modal, close it and close it when clicked outside the modal. CSS: added display:block to the show-modal class which is added when the modal is clicked, added an animation to the modal class which has a key-frame to change opacity

--- 6 - menu slider modal/script.js
+++ 6 - menu slider modal/script.js
@@ -3,5 +3,14 @@ const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 
-// Toggle navigation
+// Toggle navbar on the side
 toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Close modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Close modal when click outside the box
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false )
