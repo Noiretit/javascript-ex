bde119e16a28e484fc59d6c2c7b030f99fa6a6f0
Noiret <sergio.ko@icloud.com>
2020-10-26 23:10:20 +0100

    List if now randomly ordered: createList now maps through the richdestPeople shallow copy and adds a property sort with a random number, then it's sorted by its sort property and maps again to show the value (name of the rich) (without this it only shows object Object)

--- 19 - sortable list drag-drop API/script.js
+++ 19 - sortable list drag-drop API/script.js
@@ -22,6 +22,13 @@ let dragStartIndex;
 // Insert list items into DOM
 const createList = () => {
     [...richestPeople]
+        // map goes through the array and gives back its value and adds
+        // a new one with a Math.random (from 0 to 1 with decimals)
+        .map(each => ({ value: each, sort: Math.random() }))
+        // sort orders them by their sort property
+        .sort((a, b) => a.sort - b.sort)
+        // this map shows the value of each iteration (without it gives back [object Object])
+        .map(a => a.value)
         .forEach((person, index) => {
             const listItem = document.createElement('li');
             // When setting an attribute, you have to name it "data-" and then whatever you want
